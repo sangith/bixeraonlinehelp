6f0599c0c8af14cb7966815df155bd220630a575
dimplemanoj <dimple.poojary@gmail.com>
2015-09-12 20:26:34 +0530

    Completed Customer Module

---
+++ bixer.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Compiling Help File
+"C:\Freelance\vahelp\help-hcw\hhc.exe" "C:\FREELA~1\HELPMA~1\output\Bixera\bixer_tmphhp\bixer.hhp"
+pause
---
+++ bixer_hhp.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Compiling Help File
+"C:\Freelance\vahelp\vahelp74.exe" /ce "C:\Freelance\HelpMaker\output\Bixera\Bixera.sh6"
+pause
